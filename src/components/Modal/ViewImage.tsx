a445901ab26426c27313bed6d303ab57738ae6a0
kaikySantos <info.kaiky@gmail.com>
2022-08-05 14:39:21 -0300

    feat: cardlist and view image

--- src/components/Modal/ViewImage.tsx
+++ src/components/Modal/ViewImage.tsx
@@ -19,5 +19,25 @@ export function ModalViewImage({
   onClose,
   imgUrl,
 }: ModalViewImageProps): JSX.Element {
-  // TODO MODAL WITH IMAGE AND EXTERNAL LINK
+  return (
+    <Modal isOpen={isOpen} onClose={onClose} isCentered size="4xl">
+      <ModalOverlay />
+      <ModalContent
+        mx="auto"
+        w="100%"
+        h="100%"
+        maxWidth={['300px', '500px', '900px']}
+        maxH={['350px', '450px', '600px']}
+      >
+        <ModalBody p="0">
+          <Image src={imgUrl} w="100%" h="100%" />
+        </ModalBody>
+        <ModalFooter bg="pGray.800" h="2rem" py="20px" borderBottomRadius="5px">
+          <Link href={imgUrl} isExternal fontSize="1rem" mr="auto">
+            Abrir original
+          </Link>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
 }

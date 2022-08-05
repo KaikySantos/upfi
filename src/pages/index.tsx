1ac0ea53b34e85054e845ba6a59f6d8fb31edaf0
kaikySantos <info.kaiky@gmail.com>
2022-08-05 13:38:22 -0300

    feat: axios request with param

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,29 @@ import { api } from '../services/api';
 import { Loading } from '../components/Loading';
 import { Error } from '../components/Error';
 
+interface Image {
+  title: string;
+  description: string;
+  url: string;
+  ts: number;
+  id: string;
+}
+
+interface GetImageResponse {
+  after: string;
+  data: Image[];
+}
+
 export default function Home(): JSX.Element {
+  async function fetchImages({ pageParam = null }): Promise<GetImageResponse> {
+    const { data } = await api('/api/images', {
+      params: {
+        after: pageParam,
+      },
+    });
+    return data;
+  }
+
   const {
     data,
     isLoading,
@@ -18,8 +40,10 @@ export default function Home(): JSX.Element {
     hasNextPage,
   } = useInfiniteQuery(
     'images',
-    // TODO AXIOS REQUEST WITH PARAM
-    ,
+    fetchImages,
+    {
+      getNextPageParam: lastPage => lastPage?.after || null,
+    }
     // TODO GET AND RETURN NEXT PAGE PARAM
   );
 
